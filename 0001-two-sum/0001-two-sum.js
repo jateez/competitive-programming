49e67003e662c1c4a39b642ae3907e5c03072d9c
Arya Wibowo Jati <94813894+jateez@users.noreply.github.com>
2025-01-22 22:24:17 +0700

    Time: 35 ms (46.07%), Space: 49.5 MB (82.86%) - LeetHub

--- 0001-two-sum/0001-two-sum.js
+++ 0001-two-sum/0001-two-sum.js
@@ -4,18 +4,13 @@
  * @return {number[]}
  */
 var twoSum = function (nums, target) {
-    let left = 0;
-    let right = 1;
-
-    while (left < right) {
-        right = left + 1;
-        while (right < nums.length) {
-            if (target - nums[left] == nums[right]) {
-                return [left, right]
-            }
-            right++;
+    for (let i = 0; i < nums.length - 1; i++) {
+        let diff = target - nums[i]
+        let pointer = i + 1;
+        while (pointer < nums.length) {
+            if (nums[pointer] === diff) return [i, pointer]
+            pointer++;
         }
-        left++;
     }
-    return [];
+    return []
 };
