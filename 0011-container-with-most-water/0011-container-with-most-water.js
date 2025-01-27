c779cc785a2ddadc0ea6788115fc0fcda29df912
Arya Wibowo Jati <94813894+jateez@users.noreply.github.com>
2025-01-27 21:01:31 +0700

    Time: 3 ms (53.04%), Space: 58.2 MB (24.63%) - LeetHub

---
+++ 0011-container-with-most-water/0011-container-with-most-water.js
@@ -0,0 +1,27 @@
+/**
+ * @param {number[]} height
+ * @return {number}
+ */
+var maxArea = function (height) {
+
+    if (height.length === 2) {
+        return (height[0] > height[1] ? height[1] : height[0]) * 1
+    }
+
+    let right = height.length - 1, left = 0, currMaxAmount = 0;
+
+    while (left < right) {
+        let w = right - left;
+        let h = Math.min(height[left], height[right]);
+        let area = w * h;
+
+        currMaxAmount = Math.max(area, currMaxAmount);
+
+        if (height[left] > height[right])
+            right--;
+        else
+            left++;
+    }
+
+    return currMaxAmount;
+};
