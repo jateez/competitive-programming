a5ae239eb3112a9c59722837d7aaea13bb91cd5f
Arya Wibowo Jati <aryawjati@gmail.com>
2025-08-21 01:51:36 +0700

    Time: 3 ms (37.73%), Space: 41.4 MB (97.15%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.java
+++ 0020-valid-parentheses/0020-valid-parentheses.java
@@ -6,12 +6,12 @@ class Solution {
         map.put(')', '(');
         map.put('}', '{');
         map.put(']', '[');
-        int len = s.length();
+        var len = s.length();
         for (int i = 0; i < len; i++) {
             char c = s.charAt(i);
             if (c == '(' || c == '{' || c == '[') {
                 stack.push(c);
-            } else if (stack.empty()) {
+            } else if (stack.isEmpty()) {
                 return false;
             } else if (stack.pop() != map.get(c)) {
                 return false;
