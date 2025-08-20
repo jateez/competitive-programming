d38982fedf2a444b894bc227fb38580c3c7cd8b6
Arya Wibowo Jati <aryawjati@gmail.com>
2025-08-21 01:54:59 +0700

    0020-valid-parentheses Easy .java 08-21-2025 2 ms (96.56%) 42 MB (19.83%)

---
+++ LeetCode/Easy/0020-valid-parentheses/0020-valid-parentheses.java
@@ -0,0 +1,21 @@
+class Solution {
+    Stack<Character> stack = new Stack();
+    public boolean isValid(String s) {
+        Map<Character, Character> map = new HashMap<>();
+        map.put(')', '(');
+        map.put('}', '{');
+        map.put(']', '[');
+        var len = s.length();
+        for (int i = 0; i < len; i++) {
+            char c = s.charAt(i);
+            if (c == '(' || c == '{' || c == '[') {
+                stack.push(c);
+            } else if (stack.isEmpty()) {
+                return false;
+            } else if (stack.pop() != map.get(c)) {
+                return false;
+            }
+        }
+        return stack.isEmpty();
+    }
+}
