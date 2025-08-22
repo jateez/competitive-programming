95052c142d168a8ebb16abdc3590f0e96c0b19e2
Arya Wibowo Jati <aryawjati@gmail.com>
2025-08-22 15:59:19 +0700

    0167-two-sum-ii-input-array-is-sorted Medium .java 3 ms (24.09%) 47.7 MB (10.96%)

--- LeetCode/Medium/0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.java
+++ LeetCode/Medium/0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.java
@@ -7,13 +7,13 @@ class Solution {
         int left = 0, right = numbers.length - 1;
         while (left < right) {
             if ((numbers[left] + numbers[right]) == target) {
-                return new int[] { left + 1, right + 1 };
+                break;
             } else if ((numbers[left] + numbers[right]) > target) {
                 right--;
             } else if ((numbers[left] + numbers[right]) < target) {
                 left++;
             }
         }
-        return new int[] { 0, 0 };
+        return new int[] { left + 1, right + 1 };
     }
 }
