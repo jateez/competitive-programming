2e2e3ecad4b2598e1e1ce1ffe3132ec3eee3e363
Arya Wibowo Jati <aryawjati@gmail.com>
2025-08-22 15:14:45 +0700

    0121-best-time-to-buy-and-sell-stock Easy .java 2 ms (72.15%) 61.6 MB (74.96%)

--- LeetCode/Easy/0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.java
+++ LeetCode/Easy/0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.java
@@ -1,15 +1,12 @@
 class Solution {
     public int maxProfit(int[] prices) {
         int currMaxProfit = 0;
-        int left = 0;
         int lowestPrice = prices[0];
         int right = 1;
         while (right < prices.length) {
-            if (currMaxProfit < (prices[right] - lowestPrice)) {
-                currMaxProfit = (prices[right] - lowestPrice);
-            } else if (lowestPrice > prices[right]) {
+            currMaxProfit = Math.max(currMaxProfit, (prices[right] - lowestPrice));
+            if (lowestPrice > prices[right]) {
                 lowestPrice = prices[right];
-                left = right;
             }
             right++;
         }
