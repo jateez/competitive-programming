85c550b9e6a9bc7e72640e3cf4f1edb962d6aa03
Arya Wibowo Jati <aryawjati@gmail.com>
2025-08-22 16:20:33 +0700

    0011-container-with-most-water Medium .java 5 ms (76.32%) 58 MB (43.71%)

--- LeetCode/Medium/0011-container-with-most-water/0011-container-with-most-water.java
+++ LeetCode/Medium/0011-container-with-most-water/0011-container-with-most-water.java
@@ -3,14 +3,12 @@ class Solution {
         int left = 0, right = height.length - 1;
         int currAmount = 0;
         while (left < right) {
-            int w = right - left;
-            int h = Math.min(height[right], height[left]);
+            currAmount = Math.max(currAmount, ((right - left) * (Math.min(height[left], height[right]))));
             if (height[left] > height[right]) {
                 right--;
             } else {
                 left++;
             }
-            currAmount = Math.max(currAmount, (h * w));
         }
         return currAmount;
     }
