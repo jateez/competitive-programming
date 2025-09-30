a0a2239621ced8b8082e67216bbf216640e9af9b
Arya Wibowo Jati <aryawjati@gmail.com>
2025-09-30 19:17:08 +0700

    0042-trapping-rain-water Hard .go 0 ms (100%) 8.1 MB (59.76%)

--- LeetCode/Hard/0042-trapping-rain-water/0042-trapping-rain-water.go
+++ LeetCode/Hard/0042-trapping-rain-water/0042-trapping-rain-water.go
@@ -1,20 +1,20 @@
 func trap(height []int) int {
+	maxWater := 0
 	left := 0
 	right := len(height) - 1
 	leftWall := height[left]
 	rightWall := height[right]
-	currSum := 0
 
 	for left < right {
 		if height[left] < height[right] {
-            leftWall = max(leftWall, height[left])
-            currSum += leftWall - height[left]
-            left++
+			leftWall = max(leftWall, height[left])
+			maxWater += leftWall - height[left]
+			left++
 		} else {
 			rightWall = max(rightWall, height[right])
-            currSum += rightWall - height[right]
-            right--
+			maxWater += rightWall - height[right]
+			right--
 		}
 	}
-	return currSum
+	return maxWater
 }
