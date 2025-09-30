1dc3ce751886b26d422e3d10ae4030fd04105b3c
Arya Wibowo Jati <aryawjati@gmail.com>
2025-09-30 18:42:30 +0700

    0167-two-sum-ii-input-array-is-sorted Medium .go 0 ms (100%) 7.9 MB (71.3%)

--- LeetCode/Medium/0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.go
+++ LeetCode/Medium/0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.go
@@ -2,15 +2,16 @@ func twoSum(numbers []int, target int) []int {
 	if len(numbers) == 2 {
 		return []int{1, 2}
 	}
-	right := len(numbers) - 1
 	left := 0
+	right := len(numbers) - 1
+
 	for left < right {
-		if numbers[left]+numbers[right] == target {
-			return []int{left + 1, right + 1}
-		} else if numbers[left]+numbers[right] > target {
+		if numbers[left]+numbers[right] > target {
 			right--
-		} else {
+		} else if numbers[left]+numbers[right] < target {
 			left++
+		} else {
+			return []int{left + 1, right + 1}
 		}
 	}
 	return []int{1, 2}
