b041d9af9ddf8678b4507449134e37daeff7c90f
Arya Wibowo Jati <aryawjati@gmail.com>
2025-09-30 19:08:33 +0700

    0011-container-with-most-water Medium .go 4 ms (8.72%) 10.4 MB (21.76%)

--- LeetCode/Medium/0011-container-with-most-water/0011-container-with-most-water.go
+++ LeetCode/Medium/0011-container-with-most-water/0011-container-with-most-water.go
@@ -1,24 +1,15 @@
 func maxArea(height []int) int {
-	/**
-	  1.  calculate current area size by seeking
-	      lowest wall for left and right side multilply with distance
-	  2.  Check if curr area > previous area, if yes update
-	  3. continue increment left current left wall is
-	      higher than right wall, if not, otherwise
-	*/
-
 	left := 0
 	right := len(height) - 1
-	currAmount := float64(0)
-
+	currMaxArea := 0
 	for left < right {
-		temp := float64(right-left) * math.Min(float64(height[left]), float64(height[right]))
-		currAmount = math.Max(currAmount, temp)
+		temp := (right - left) * (min(height[left], height[right]))
+		currMaxArea = max(currMaxArea, temp)
 		if height[left] > height[right] {
 			right--
 		} else {
 			left++
 		}
 	}
-	return int(currAmount)
+	return currMaxArea
 }
