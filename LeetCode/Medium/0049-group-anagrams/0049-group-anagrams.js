46b98de05c56ecb511889df7bc9ec37107cda202
Arya Wibowo Jati <aryawjati@gmail.com>
2025-10-01 12:30:36 +0700

    0049-group-anagrams Medium .js N/A (0%) N/A (0%)

---
+++ LeetCode/Medium/0049-group-anagrams/0049-group-anagrams.js
@@ -0,0 +1,13 @@
+/**
+ * @param {string[]} strs
+ * @return {string[][]}
+ */
+var groupAnagrams = function (strs) {
+    if (strs.length === 1) return [strs]
+    const ans = []
+
+    let temp = [strs[0]]
+    for (let i = 0; i < strs.length - 1; i++) {
+        if (){}
+    }
+};
