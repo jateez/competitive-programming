60e8bc3844b1932cba40e4c18fd68863524e50c8
Arya Wibowo Jati <aryawjati@gmail.com>
2026-02-19 21:11:07 +0700

    0049-group-anagrams Medium .js 28 ms (76.21%) 69.7 MB (16.55%)

--- LeetCode/Medium/0049-group-anagrams/0049-group-anagrams.js
+++ LeetCode/Medium/0049-group-anagrams/0049-group-anagrams.js
@@ -3,11 +3,20 @@
  * @return {string[][]}
  */
 var groupAnagrams = function (strs) {
-    if (strs.length === 1) return [strs]
-    const ans = []
+    const map = new Map()
 
-    let temp = [strs[0]]
-    for (let i = 0; i < strs.length - 1; i++) {
-        if (){}
+    for (let word of strs) {
+        const count = new Array(26).fill(0)
+
+        for (let char of word) {
+            count[char.charCodeAt(0) - 97]++;
+        }
+        const key = count.join('#')
+
+        if (!map.has(key)) {
+            map.set(key, [])
+        }
+        map.get(key).push(word)
     }
+    return Array.from(map.values())
 };
