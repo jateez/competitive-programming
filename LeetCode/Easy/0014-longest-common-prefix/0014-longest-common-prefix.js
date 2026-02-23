d0cd68a9d4b7de428a2b81314a2f855ac36d0a21
Arya Wibowo Jati <aryawjati@gmail.com>
2026-02-23 18:38:52 +0700

    0014-longest-common-prefix Easy .js 1 ms (59.82%) 53.2 MB (95.8%)

--- LeetCode/Easy/0014-longest-common-prefix/0014-longest-common-prefix.js
+++ LeetCode/Easy/0014-longest-common-prefix/0014-longest-common-prefix.js
@@ -6,10 +6,10 @@ var longestCommonPrefix = function (strs) {
 
     strs.sort()
     let ans = ""
-    const firstWord = strs[0], lastWord = strs[strs.length - 1]
+    let firstWord = strs[0], lastWord = strs[strs.length - 1]
     for (let i = 0; i < Math.min(firstWord.length, lastWord.length); i++) {
         if (firstWord[i] !== lastWord[i]) {
-            return ans
+            break
         } else {
             ans += firstWord[i]
         }
